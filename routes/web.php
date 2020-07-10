7f293b1a566b3e3a45dd910f0ffc16fcc68b97e8
Thalibe Ferreira <thalibe55@gmail.com>
2020-07-10 16:19:15 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/series', 'SeriesController@index')->name('listar_series');
+Route::get('/series/criar', 'SeriesController@create')->name('form_criar_series');
+Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+
+Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
